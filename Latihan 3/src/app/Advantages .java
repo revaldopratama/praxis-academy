0b5d86862e7a77fd8bdaaff70a2ec481631b5c6c
Revaldo Pratama <revaldopratama019@gmail.com>
2019-09-05 16:44:01 +0700

    update

---
+++ Latihan 3/src/app/Advantages .java
@@ -0,0 +1,22 @@
+package app;
+
+import java.util.*; 
+
+class Advantages 
+{ 
+	public static void main(String[] args) 
+	{ 
+		// Creatinga an ArrayList without any type specified 
+        ArrayList al = new ArrayList(); 
+  
+        al.add("Sachin"); 
+        al.add("Rahul"); 
+        al.add(10); // Compiler allows this 
+  
+        String s1 = (String)al.get(0); 
+        String s2 = (String)al.get(1); 
+  
+        // Causes Runtime Exception 
+        String s3 = (String)al.get(2); 
+	} 
+} 
